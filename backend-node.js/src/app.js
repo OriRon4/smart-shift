01cca534bb6b40a3ddeb706b9596c8226af71019
OriRon4 <oriron06@gmail.com>
2026-04-19 20:49:33 +0300

    Reapply "Reapply "commit ns?""
    This reverts commit 906db73b0303cf8ec27db952d00a261c9728b741.

--- backend-node.js/src/app.js
+++ backend-node.js/src/app.js
@@ -17,7 +17,9 @@ app.use((req, res) => {
 
 app.use((error, req, res, next) => {
   console.error(error);
-  res.status(500).json({ error: error.message || "Internal server error" });
+  res
+    .status(error.statusCode || 500)
+    .json({ error: error.message || "Internal server error" });
 });
 
 module.exports = app;
